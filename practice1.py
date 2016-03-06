e330cc2eac7eff5cdcbe1f54379375703f51f93a
Siena Biales <siena.biales@uconn.edu>
2016-03-05 22:35:37 -0500

    done?????

--- practice1.py
+++ practice1.py
@@ -2,8 +2,11 @@
 #(0,0) is exactly in the middle of the lattice (50 steps in each direction)
 #simulation stops when walker is trapped (ie all directions visited or border)
 
+import random
+
 size = 100
 lattice = {}
+cont = True
 
 #Initialize all locations on lattice to "unvisited"
 for x in range(-(size/2), (size/2)) :
@@ -16,33 +19,47 @@ lattice[(0,0)] = 1
 curx = 0
 cury = 0
 
-moves = []
-#Create a list of the available next moves
-#check left
-x = curx - 1
-y = cury
-if lattice[(x,y)] = 0
-    moves.append((x,y))
+while(contin) :
 
-#check right
-x = curx + 1
-y = cury
-if lattice[(x,y)] = 0
-    moves.append((x,y))
+    moves = []
+    #Create a list of the available next moves
+    #check left
+    x = curx - 1
+    y = cury
+    if lattice[(x,y)] == 0 :
+        moves.append((x,y))
 
-#check up
-x = curx
-y = cury + 1
-if lattice[(x,y)] = 0
-    moves.append((x,y))
+    #check right
+    x = curx + 1
+    y = cury
+    if lattice[(x,y)] == 0 :
+        moves.append((x,y))
 
-#check down
-x = curx
-y = cury - 1
-if lattice[(x,y)] = 0
-    moves.append((x,y))
+    #check up
+    x = curx
+    y = cury + 1
+    if lattice[(x,y)] == 0 :
+        moves.append((x,y))
 
+    #check down
+    x = curx
+    y = cury - 1
+    if lattice[(x,y)] == 0 :
+        moves.append((x,y))
 
+    if moves == [] :
+        contin = False
+        continue
+
+    #choose one move at random
+    move = random.choice(moves)
+
+    #update current location
+    curx = move[0]
+    cury = move[1]
+
+    #mark it as visited
+    lattice[move] = 1
 
 
 

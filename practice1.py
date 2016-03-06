4a1d891c399dc6c0f33b5804efc3c2f528634817
Siena Biales <siena.biales@uconn.edu>
2016-03-05 22:07:57 -0500

    Update

--- practice1.py
+++ practice1.py
@@ -10,4 +10,42 @@ for x in range(-(size/2), (size/2)) :
     for y in range(-(size/2), (size/2)) :
         lattice[(x,y)] = 0
 
-#Set (0,0)
+#Set (0,0) to "visited"
+lattice[(0,0)] = 1
+
+curx = 0
+cury = 0
+
+moves = []
+#Create a list of the available next moves
+#check left
+x = curx - 1
+y = cury
+if lattice[(x,y)] = 0
+    moves.append((x,y))
+
+#check right
+x = curx + 1
+y = cury
+if lattice[(x,y)] = 0
+    moves.append((x,y))
+
+#check up
+x = curx
+y = cury + 1
+if lattice[(x,y)] = 0
+    moves.append((x,y))
+
+#check down
+x = curx
+y = cury - 1
+if lattice[(x,y)] = 0
+    moves.append((x,y))
+
+
+
+
+
+
+    
+

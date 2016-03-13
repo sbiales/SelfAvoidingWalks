73375968c82fb79fb206e2a870553828b8faaf78
Vincent Chov <vincentchov@gmail.com>
2016-03-13 18:55:10 -0400

    Commented the code and added indentations to address indentation errors.  Also put everything in a for loop to run simulation 100 times and compute mean walk distance

--- practice1.py
+++ practice1.py
@@ -3,75 +3,88 @@
 #simulation stops when walker is trapped (ie all directions visited or border)
 
 import random
+import numpy
 
-size = 100
-lattice = {}
-contin = True
-length = 0
+lengthList = []		# List holding walk lengths for each simulation
+size = 100		# Size of lattice, used to initialize lattice
 
-#Initialize all locations on lattice to "unvisited"
-for x in range(int(-(size/2)), int(size/2)) :
-    for y in range(int(-(size/2)), int(size/2)) :
-        lattice[(x,y)] = 0
+# Run 100 simulations
+for i in range(1, 100): 
+        lattice = {}		# Initialize the lattice as an empty dictionary
+        contin = True		# Control variable for while loop
+        length = 0		# Length of walk
 
-#Set (0,0) to "visited"
-lattice[(0,0)] = 1
-length += 1
 
-curx = 0
-cury = 0
+        #Initialize all locations on lattice to "unvisited", where 0 means unvisited, and 1 means visited
+        for x in range(int(-(size/2)), int(size/2)) :
+            for y in range(int(-(size/2)), int(size/2)) :
+                lattice[(x,y)] = 0
 
-while(contin) :
+        #Set (0,0) to "visited"
+        lattice[(0,0)] = 1
+        length += 1
 
-    moves = []
-    #Create a list of the available next moves
-    #check left
-    x = curx - 1
-    y = cury
-    if (x,y) in lattice :
-        if lattice[(x,y)] == 0 :
-            moves.append((x,y))
+        curx = 0		# Initialize current x-coordinate to 0
+        cury = 0		# Initialize current y-coordinate to 0
 
-    #check right
-    x = curx + 1
-    if (x,y) in lattice :
-        if lattice[(x,y)] == 0 :
-            moves.append((x,y))
+        while(contin) :
 
-    #check up
-    x = curx
-    y = cury + 1
-    if (x,y) in lattice :
-        if lattice[(x,y)] == 0 :
-            moves.append((x,y))
+                moves = []		#Create a list of the available next moves, which is initially empty
 
-    #check down
-    y = cury - 1
-    if (x,y) in lattice :
-        if lattice[(x,y)] == 0 :
-            moves.append((x,y))
+                # x and y denote temporary positions in the lattice that we'll use to check if the position next to us is available
+                # First check to see if this is a valid position (it's in the lattice)
+                # Then if this position is unvisited, add this position as a tuple to the list of available moves
 
-    if moves == [] :
-        contin = False
-        continue
+                #check left
+                x = curx - 1
+                y = cury
+                if (x,y) in lattice :		 
+                        if lattice[(x,y)] == 0 :	
+                            moves.append((x,y))
 
-    #choose one move at random
-    move = random.choice(moves)
+                #check right
+                x = curx + 1
+                if (x,y) in lattice :
+                        if lattice[(x,y)] == 0 :
+                            moves.append((x,y))
 
-    #update current location
-    curx = move[0]
-    cury = move[1]
+                #check up
+                x = curx
+                y = cury + 1
+                if (x,y) in lattice :
+                        if lattice[(x,y)] == 0 :
+                            moves.append((x,y))
 
-    #mark it as visited
-    lattice[move] = 1
-    length += 1
+                #check down
+                y = cury - 1
+                if (x,y) in lattice :
+                        if lattice[(x,y)] == 0 :
+                            moves.append((x,y))
+
+                # Now that we've made a list of available moves, let's check to see if there are any available moves.
+                if moves == [] :			# If there are no available moves then, we quit
+                        contin = False
+                        continue
+                else :				# If there are indeed available moves, choose one at random
+                        #choose one move at random
+                        move = random.choice(moves)
+
+                        #update current location
+                        curx = move[0]			# curx = x-position of randomly chosen move tuple
+                        cury = move[1]			# cury = y-position of randomly chosen move tuple
+
+                        #mark that randomly chosen position as visited in the lattice
+                        lattice[move] = 1
+                        length += 1			# Since we moved to that position, increment the length of the walk
     
-    print(move)
+        print("Chose position", move)	# Display the position we chose to move to
+        # Continue with the while loop until there are no available moves
 
-print(length, " steps taken")
+        # Now that are no available moves left, output the length of the walk
+        print(length, "steps taken in this walk")
 
+        # Now that this simulation is over, append the length of this walk to the list of walk lengths
+        lengthList.append(length)
 
-
-
-    
+print(numpy.mean(lengthList))
 

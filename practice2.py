7484e59ee8569309c63ef3546febd70be5699071
Siena Biales <siena.biales@uconn.edu>
2016-03-13 00:16:33 -0500

    P2 can calculate distance and size of lattice

--- practice2.py
+++ practice2.py
@@ -2,66 +2,86 @@
 
 import random
 
-steps = 5000
-lattice = []
-contin = True
+iterations = 10
+distances = []
+sizes = []
+remaining = iterations
+
+while (remaining > 0) :
+    steps = 5000
+    lattice = []
+    contin = True
 
 
-#set (0,0) to "visited"
-lattice.append((0,0))
+    #set (0,0) to "visited"
+    lattice.append((0,0))
 
-curx = 0
-cury = 0
+    curx = 0
+    cury = 0
 
-while(contin) :
-    moves = []
+    maxx = 0
+    maxy = 0
+    minx = 0
+    miny = 0
 
-    #create a list of the available next moves
-    #check left
-    x = curx - 1
-    y = cury
-    if (x,y) not in lattice :
-        moves.append((x,y))
+    while(contin) :
+        moves = []
 
-    #check right
-    x = curx + 1
-    if (x,y) not in lattice :
-        moves.append((x,y))
+        #create a list of the available next moves
+        #check left
+        x = curx - 1
+        y = cury
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    #check up
-    x = curx
-    y = cury + 1
-    if (x,y) not in lattice :
-        moves.append((x,y))
+        #check right
+        x = curx + 1
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    #check down
-    y = cury - 1
-    if (x,y) not in lattice :
-        moves.append((x,y))
+        #check up
+        x = curx
+        y = cury + 1
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    if moves == [] :
-        contin = False
-        print("No available moves")
-        continue
+        #check down
+        y = cury - 1
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    if steps == 0 :
-        contin = False
-        print("No more steps")
-        continue
+        if moves == [] :
+            contin = False
+            print("No available moves")
+            
+            continue
 
-    #choose one move at random
-    move = random.choice(moves)
+        if steps == 0 :
+            contin = False
+            print("No more steps")
+            continue
 
-    #update current location
-    curx = move[0]
-    cury = move[1]
+        #choose one move at random
+        move = random.choice(moves)
 
-    #mark it as visited
-    lattice.append(move)
+        #update current location
+        curx = move[0]
+        cury = move[1]
 
-    steps = steps - 1
+        #mark it as visited
+        lattice.append(move)
 
-    print(move)
+        #check if it replaces a max or min
+        maxx = max(maxx, curx)
+        minx = min(minx, curx)
+        maxy = max(maxy, cury)
+        miny = min(miny, cury)
 
-length = 5000 - steps
-print(length, " steps taken")
+        steps -= 1
+    
+    remaining -= 1
+    distance = abs(0-curx) + abs(0-cury)
+    size = (maxx-minx)*(maxy-miny)
+
+    print("Iteration #", (iterations-remaining), "\t : Distance ", distance, ", Lattice size ", size)
+

b406c6d9ed8758d358e1f83d71e02001f527d60d
Vincent Chov <vincentchov@gmail.com>
2016-03-14 15:40:28 -0400

    Renamed Siena's practice 2 and made practice 2 ready to finish question 4.  Time to clean up the code?

--- practice2.py
+++ practice2.py
@@ -1,67 +1,113 @@
-#self avoiding random walk of 5000 steps, no border constraints
+# simulate self avoiding walk in indefinite lattice
+# (0,0) is exactly in the middle of the lattice (50 steps in each direction)
+# simulation stops when walker is trapped (ie all directions visited or border)
+# max of 5000 steps
 
 import random
+import numpy
 
-steps = 5000
-lattice = []
-contin = True
+lengthList = []		    # List holding walk lengths for each simulation
+distanceList = []       # List holding the distances from starting point for each simulation
+latticeSizeList = []    # List holding the lattice sizes for each simulation
 
+# Run 10 simulations
+for i in range(1, 10):
+    lattice =  []		# Initialize the lattice as an empty list of points visited
+    length = 0		    # Length of a single walk, used as a control variable
 
-#set (0,0) to "visited"
-lattice.append((0,0))
+    #Set (0,0), the starting point, to "visited" by appending it to the list of known lattice points
+    lattice.append((0,0))
+    length += 1
 
-curx = 0
-cury = 0
+    startx = 0          # Keep track of starting x-coordinate
+    starty = 0          # Keep track of starting y-coordinate
+    curx = startx		# Initialize current x-coordinate to 0
+    cury = starty		# Initialize current y-coordinate to 0
 
-while(contin) :
-    moves = []
+    while(length <= 5000) :
+        moves = []		#Create a list of the available next moves, which is initially empty
 
-    #create a list of the available next moves
-    #check left
-    x = curx - 1
-    y = cury
-    if (x,y) not in lattice :
-        moves.append((x,y))
+        # x and y denote temporary positions in the lattice that we'll use to check if the position next to us is available
+        # First check to see if this is a valid position (it's in the lattice)
+        # Then if this position is unvisited, add this position as a tuple to the list of available moves
 
-    #check right
-    x = curx + 1
-    if (x,y) not in lattice :
-        moves.append((x,y))
+        #check left
+        x = curx - 1
+        y = cury
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    #check up
-    x = curx
-    y = cury + 1
-    if (x,y) not in lattice :
-        moves.append((x,y))
+        #check right
+        x = curx + 1
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    #check down
-    y = cury - 1
-    if (x,y) not in lattice :
-        moves.append((x,y))
+        #check up
+        x = curx
+        y = cury + 1
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    if moves == [] :
-        contin = False
-        print("No available moves")
-        continue
+        #check down
+        y = cury - 1
+        if (x,y) not in lattice :
+            moves.append((x,y))
 
-    if steps == 0 :
-        contin = False
-        print("No more steps")
-        continue
+        # Now that we've made a list of available moves, let's check to see if there are any available moves.
+        if moves == [] :			# If there are no available moves then, we quit
+            print("No more available moves")
+            break
+            #continue
 
-    #choose one move at random
-    move = random.choice(moves)
+        else :				# If there are indeed available moves, choose one at random
+            #choose one move at random
+            move = random.choice(moves)
 
-    #update current location
-    curx = move[0]
-    cury = move[1]
+            #update current location
+            curx = move[0]			# curx = x-position of randomly chosen move tuple
+            cury = move[1]			# cury = y-position of randomly chosen move tuple
 
-    #mark it as visited
-    lattice.append(move)
+            # add that randomly chosen move to the lattice to indicate that it was visited
+            lattice.append(move)
+            length += 1			# Since we moved to that position, increment the length of the walk
 
-    steps = steps - 1
+            print("Chose position", move)	# Display the position we chose to move to
+            # Continue with the while loop until there are no available moves
 
-    print(move)
+    # Now that this simulation is over, let's run some calculations and output stats about this walk
+    # The last known current x and last known current y is that end value for each
+    endx = curx
+    endy = cury
 
-length = 5000 - steps
-print(length, " steps taken")
+    # append the length of this walk to the list of walk lengths
+    lengthList.append(length)
+
+    # Calculate distance (distance = \x_start - x_end\ + \y_start - x_end\) and add to list of distances
+    distance = abs(endx-startx) + abs(endy-starty)
+    distanceList.append(distance)
+
+    # Sort the lattice size by x coordinates and y coordinates to calculate lattice size
+    # Then calculate the lattice size for this walk (size = (x_max - x_min)*(y_max - y_min)) and add it to the list
+    lattice_size_x = sorted(lattice, key=lambda tup: tup[0])
+    lattice_size_y = sorted(lattice, key=lambda tup: tup[1])
+
+    lattice_size = ((lattice_size_x[-1])[0]-(lattice_size_x[0])[0]) * ((lattice_size_y[-1])[1]-(lattice_size_y[0])[1])
+    latticeSizeList.append(lattice_size)
+
+    # Now print the stats for this walk
+    print("Walk length:", length)
+    print("Distance in this walk:", distance)
+    print("Lattice size in this walk:", lattice_size)
+
+    # Now continue the for loop until all 100 iterations are done
+
+# Now that the 100 iterations are done, print the stats for distance and lattice size
+print("Mean distance:",numpy.mean(distanceList))
+print("Median distance:",numpy.median(distanceList))
+print("Variance for distance",numpy.var(distanceList))
+print("Standard deviation for distance",numpy.std(distanceList))
+
+print("Mean lattice size:",numpy.mean(latticeSizeList))
+print("Median lattice size:",numpy.median(latticeSizeList))
+print("Variance for lattice size",numpy.var(latticeSizeList))
+print("Standard deviation for lattice size",numpy.std(latticeSizeList))
---
+++ practice2_old.py
@@ -0,0 +1,68 @@
+#self avoiding random walk of 5000 steps, no border constraints
+
+import random
+import numpy
+
+steps = 5000
+lattice = []
+contin = True
+
+
+#set (0,0) to "visited"
+lattice.append((0,0))
+
+curx = 0
+cury = 0
+
+while(contin) :
+    moves = []
+
+    #create a list of the available next moves
+    #check left
+    x = curx - 1
+    y = cury
+    if (x,y) not in lattice :
+        moves.append((x,y))
+
+    #check right
+    x = curx + 1
+    if (x,y) not in lattice :
+        moves.append((x,y))
+
+    #check up
+    x = curx
+    y = cury + 1
+    if (x,y) not in lattice :
+        moves.append((x,y))
+
+    #check down
+    y = cury - 1
+    if (x,y) not in lattice :
+        moves.append((x,y))
+
+    if moves == [] :
+        contin = False
+        print("No available moves")
+        continue
+
+    if steps == 0 :
+        contin = False
+        print("No more steps")
+        continue
+
+    #choose one move at random
+    move = random.choice(moves)
+
+    #update current location
+    curx = move[0]
+    cury = move[1]
+
+    #mark it as visited
+    lattice.append(move)
+
+    steps = steps - 1
+
+    print(move)
+
+length = 5000 - steps
+print(length, " steps taken")

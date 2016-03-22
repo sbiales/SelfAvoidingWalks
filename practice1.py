274dcf07fe53443d43249215b2501470d76865f9
sib12004 <siena.biales@uconn.edu>
2016-03-22 10:03:21 -0400

    Added comments

--- practice1.py
+++ practice1.py
@@ -5,39 +5,44 @@
 import random
 import math
 
-size = 100
-lattice = {}
-contin = True
+size = 100                  #The desired height and width of the lattice
+lattice = {}                #Initializes an empty dictionary to represent the lattice
+contin = True               #sets while loop variable to true
 
-iterations = 100
-pathLengths = []
+iterations = 100            #number of simulations we wish to run
+pathLengths = []            #a list that will hold the lengths of each iteration's path
 remaining = iterations
 
-while (remaining > 0) :
-    length = 0
+while (remaining > 0) :     #while there are more iterations to be run
+    length = 0              #reset the path length to 0
     contin = True
-    #Initialize all locations on lattice to "unvisited"
+    
+    #Initialize all locations on lattice to "unvisited", represented by (x,y):0 in the dictionary
     for x in range(int(-(size/2)), int(size/2)) :
         for y in range(int(-(size/2)), int(size/2)) :
             lattice[(x,y)] = 0
 
     #Set (0,0) to "visited"
     lattice[(0,0)] = 1
-    length += 1
+    length += 1             #increment the path length
 
-    curx = 0
-    cury = 0
+    curx = 0		# Initialize current x-coordinate to 0
+    cury = 0		# Initialize current y-coordinate to 0
 
     while(contin) :
 
-        moves = []
-        #Create a list of the available next moves
+        moves = []          #Create a list of the available next moves, initially empty
+
+        # x and y denote temporary positions in the lattice that we'll use to check if the position next to us is available
+        # First check to see if this is a valid position (it's in the lattice)
+        # Then if this position is unvisited, add this position as a tuple to the list of available moves
+        
         #check left
         x = curx - 1
         y = cury
-        if (x,y) in lattice :
-            if lattice[(x,y)] == 0 :
-                moves.append((x,y))
+        if (x,y) in lattice :                   #checks if (x,y) is within the lattice bounds
+            if lattice[(x,y)] == 0 :            #checks if the location is unvisited
+                moves.append((x,y))             #if within bounds and unvisited, add it to the list of available moves
 
         #check right
         x = curx + 1
@@ -58,9 +63,10 @@ while (remaining > 0) :
             if lattice[(x,y)] == 0 :
                 moves.append((x,y))
 
-        if moves == [] :
+        if moves == [] :                        #if there are no available moves, stop the while loop
             contin = False
             continue
+        #At this point there IS at least one available move
 
         #choose one move at random
         move = random.choice(moves)
@@ -69,15 +75,15 @@ while (remaining > 0) :
         curx = move[0]
         cury = move[1]
 
-        #mark it as visited
+        #mark it as visited and increment the path length
         lattice[move] = 1
         length += 1
         
         #print(move)
-    remaining -= 1
+    remaining -= 1          #decrement the number of remaining iterations
     
     print("Iteration #", (iterations-remaining), "\t : ", length, " steps taken")
-    pathLengths.append(length)
+    pathLengths.append(length)          #add the path length of the current iteration to the list
 
 #calculations
 
@@ -94,6 +100,5 @@ for l in pathLengths :
 diffsMean = sum(sqDiffs)/iterations
 std = math.sqrt(diffsMean)
 print("Standard deviation: ", std)
-
 
 
